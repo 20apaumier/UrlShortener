42f251285f859355b61f91488d14c648aa2eea03
AJ Paumier <39337835+20apaumier@users.noreply.github.com>
2024-10-01 15:26:31 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,106 @@
+using Orleans.Runtime;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.UseOrleans(siloBuilder =>
+{
+    siloBuilder.UseLocalhostClustering();
+    siloBuilder.AddMemoryGrainStorage("urls");
+});
+
+using var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/shorten",
+    static async (IGrainFactory grains, HttpRequest request, string url) =>
+    {
+        var host = $"{request.Scheme}://{request.Host.Value}";
+
+        // Validate the URL query string.
+        if (string.IsNullOrWhiteSpace(url) &&
+            Uri.IsWellFormedUriString(url, UriKind.Absolute) is false)
+        {
+            return Results.BadRequest($"""
+                The URL query string is required and needs to be well formed.
+                Consider, ${host}/shorten?url=https://www.microsoft.com.
+                """);
+        }
+
+        // Create a unique, short ID
+        var shortenedRouteSegment = Guid.NewGuid().GetHashCode().ToString("X");
+
+        // Create and persist a grain with the shortened ID and full URL
+        var shortenerGrain =
+            grains.GetGrain<IUrlShortenerGrain>(shortenedRouteSegment);
+
+        await shortenerGrain.SetUrl(url);
+
+        // Return the shortened URL for later use
+        var resultBuilder = new UriBuilder(host)
+        {
+            Path = $"/go/{shortenedRouteSegment}"
+        };
+
+        return Results.Ok(resultBuilder.Uri);
+    });
+
+// handles redirects
+app.MapGet("/go/{shortenedRouteSegment:required}",
+    static async (IGrainFactory grains, string shortenedRouteSegment) =>
+    {
+        // Retrieve the grain using the shortened ID and url to the original URL
+        var shortenerGrain =
+            grains.GetGrain<IUrlShortenerGrain>(shortenedRouteSegment);
+
+        var url = await shortenerGrain.GetUrl();
+
+        // Handles missing schemes, defaults to "http://".
+        var redirectBuilder = new UriBuilder(url);
+
+        return Results.Redirect(redirectBuilder.Uri.ToString());
+    });
+
+app.Run();
+
+public interface IUrlShortenerGrain : IGrainWithStringKey
+{
+    Task SetUrl(string fullUrl);
+
+    Task<string> GetUrl();
+}
+
+public sealed class UrlShortenerGrain(
+    [PersistentState(
+        stateName: "url",
+        storageName: "urls")]
+        IPersistentState<UrlDetails> state)
+    : Grain, IUrlShortenerGrain
+{
+    private KeyValuePair<string, string> _cache;
+
+    public async Task SetUrl(string fullUrl)
+    {
+        state.State = new()
+        {
+            ShortenedRouteSegment = this.GetPrimaryKeyString(),
+            FullUrl = fullUrl
+        };
+
+        await state.WriteStateAsync();
+    }
+
+    public Task<string> GetUrl() =>
+        Task.FromResult(_cache.Value);
+}
+
+[GenerateSerializer]
+public sealed record class UrlDetails
+{
+    [Id(0)]
+    public string FullUrl { get; set; } = "";
+
+    [Id(1)]
+    public string ShortenedRouteSegment { get; set; } = "";
+}
+
